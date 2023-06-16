1766b5b8c7c158afade9335410d11eb3f5ba233d
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2023-06-16 11:37:02 +0330

    Data fetching

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -2,8 +2,24 @@ import React from "react";
 
 interface IProps {}
 
-const Page: React.FC<IProps> = (props) => {
-  return <div>POSTS</div>;
+const Page: React.FC<IProps> = async (props) => {
+  //! Next default behaviour is Static Site Generation(SSG)
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "no-store", //! Server Side Rendering(SSR)
+    // next: { revalidate: 10 }, //! Incremental Static Regeneration (ISR)
+  });
+
+  const posts = await response.json();
+
+  return (
+    <div>
+      {posts.map((post: any) => (
+        <div key={post.id}>
+          {post.id}: {post.title}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Page;
